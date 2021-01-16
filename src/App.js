73033828c11c458fe38bb223d23556d49d2a5373
unknown <shiva.kumar@onlineshaster.com>
2021-01-16 00:03:57 -0800

    initail commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container, Grid } from "@material-ui/core";
+import React from "react";
+import "./App.css";
+import Profile from './pages/Profile';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Container>
+      <Grid container>
+        <Grid
+          item
+          xs={12}
+          sm={12}
+          md={4}
+          lg={3}
+          style={{ backgroundColor: "red" }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+         <Profile/>
+        </Grid>
+        <Grid item xs style={{ backgroundColor: "#ccc" }}>
+         {/* header */}
+         {/* portfolie */}
+         {/* footer */}
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
